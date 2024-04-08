d509e0cb4b492821ae9d9a0ed399e3c82f8af41a
David Anderson <davidanderson@live.ie>
2024-04-08 14:31:16 +0000

    Adds a Post model

--- codestar/settings.py
+++ codestar/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-010001000100-djangoblog-xxsynss2vo3.ws-eu110.gitpod.io', 'django-blog-new-21411e0a25f2.herokuapp.com']
 
@@ -86,11 +86,15 @@ WSGI_APPLICATION = 'codestar.wsgi.application'
 #    }
 #}
 
-
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.gitpod.io",
+    "https://*.herokuapp.com",
+    "https://*.codeanyapp.com"
+]
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
