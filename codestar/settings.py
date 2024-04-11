7cbb789013ba2ce39f0cf4afddb6031c2ce9ce6b
David Anderson <davidanderson@live.ie>
2024-04-11 13:50:09 +0000

    Add gitpod.io url to ALLOWED_HOSTS

--- codestar/settings.py
+++ codestar/settings.py
@@ -28,9 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['8000-010001000100-djangoblog-xxsynss2vo3.ws-eu110.gitpod.io', 'django-blog-new-21411e0a25f2.herokuapp.com']
+ALLOWED_HOSTS = ['8000-010001000100-djangoblog-i0cirhfxahu.ws-eu110.gitpod.io', 'django-blog-new-21411e0a25f2.herokuapp.com']
 
 
 # Application definition
