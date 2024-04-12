db3233c3e6548e40eeae171811ce773fbde282e9
David Anderson <davidanderson@live.ie>
2024-04-12 13:14:23 +0000

    Add the base template and home

--- codestar/settings.py
+++ codestar/settings.py
@@ -16,9 +16,9 @@ import dj_database_url
 if os.path.isfile('env.py'):
     import env
 
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 
 # Quick-start development settings - unsuitable for production
@@ -30,8 +30,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['8000-010001000100-djangoblog-a0eel1qrudw.ws-eu110.gitpod.io', 'django-blog-new-21411e0a25f2.herokuapp.com']
-
+ALLOWED_HOSTS = ['8000-010001000100-djangoblog-kdjtmfwns56.ws-eu110.gitpod.io', 'django-blog-new-21411e0a25f2.herokuapp.com']
 
 # Application definition
 
@@ -61,7 +60,7 @@ ROOT_URLCONF = 'codestar.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -132,6 +131,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
