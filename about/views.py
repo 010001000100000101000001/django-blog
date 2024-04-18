24e7e6b3df942ec5884bd02ef06ce85866b15939
David Anderson <davidanderson@live.ie>
2024-04-18 16:38:05 +0000

    Add doc strings to views

--- about/views.py
+++ about/views.py
@@ -4,6 +4,22 @@ from .models import About
 from .forms import CollaborateForm
 
 def about_me(request):
+    """
+    Renders the most recent information on the website author
+    and allows user collaboration requests.
+
+    Displays an individual instance of :model:`about.About`.
+
+    **Context**
+    ``about``
+        The most recent instance of :model:`about.About`.
+        ``collaborate_form``
+            An instance of :form:`about.CollaborateForm`.
+    
+    **Template**
+    :template:`about/about.html`
+
+    """
     # Check if the request method is POST
     if request.method == "POST":
         # If it is a POST request, populate the CollaborateForm with the POST data
