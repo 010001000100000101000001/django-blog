4702a354992b30789a5a3c0ec4866970972128d2
David Anderson <davidanderson@live.ie>
2024-04-18 16:18:07 +0000

    Grouped imports to standard library imports, third-party imports and local imports

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, reverse
 from django.views import generic
 from django.contrib import messages
 from django.http import HttpResponseRedirect
-from django.urls import reverse
 from .models import Post, Comment
 from .forms import CommentForm
 
